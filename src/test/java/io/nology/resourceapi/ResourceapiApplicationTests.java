78145b7eb7a5c60db5351cbe6966c9bd28aaf736
iti tripathy <iti.tripathy@gmail.com>
2023-01-14 19:54:48 +1100

    Initial Commit

---
+++ src/test/java/io/nology/resourceapi/ResourceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.resourceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
